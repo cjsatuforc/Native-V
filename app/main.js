4a2742a26fa145673ce3cf9b2c5e455fccacb0ee
Alexander Turin <yo@alexturin.co>
2017-07-25 11:17:28 -0700

    ssss

--- app/main.js
+++ app/main.js
@@ -8,6 +8,7 @@ const os = require('os');
 var path = require('path')
 var fs = require('fs');
 
+
 //Menubar
 var menubar = require('menubar')
 var mb = menubar({
@@ -39,19 +40,31 @@ app.on('ready', function() {
 
     logger.debug("Starting application");
 
+    // var mainWindow = new BrowserWindow({
+    //     width: 950,
+    //     height: 550,
+    //     toolbar: true,
+    //     title: "Native",
+    //     transparent: true,
+    //     frame: true,
+    //     titleBarStyle: 'hidden-inset',
+    // });
+
     var mainWindow = new BrowserWindow({
-        width: 950,
-        height: 550,
-        toolbar: true,
-        title: "Native",
-        transparent: true,
-        frame: true,
-        titleBarStyle: 'hidden-inset'
+        titleBarStyle: 'hidden',
+        transparent:   true,
+        hasShadow:     true,
+        vibrancy:      'dark'
     });
+
+
     mainWindow.loadURL('file://' + __dirname + "/index.html");
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
+
+    // electronVibrancy.SetVibrancy(mainWindow, 0);
+
     mainWindow.openDevTools();
 
     // mainWindow.webContents.once("loaded", function () {
@@ -83,10 +96,6 @@ app.on('ready', function() {
     })
 
 
-    // var nativeHandleBuffer = mainWindow.getNativeWindowHandle();
-    // var electronVibrancy = require(path.join(__dirname,'..','..'));
-    // electron.remote.getCurrentWindow().setVibrancy('medium-light')
-    // SetVibrancy(mainWindow, 6)
 
     prefsWindow.loadURL('file://' + __dirname + '/settings.html');
     // prefsWindow.show();
