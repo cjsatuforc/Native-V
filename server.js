fa94adf8e705c2381b38f76c1b5b39bc2fec4e83
Alexander Turin <yo@alexturin.co>
2017-08-01 10:47:27 -0700

    testo

--- server.js
+++ server.js
@@ -17,22 +17,22 @@ server.listen(port, function () {
 
 app.use(express.static(__dirname + '/app'));
 
-var SerialPort = require('serialport');
-var serialport = new SerialPort("/dev/cu.usbmodem1451", {
-    baudRate: 57600,
-    parser: SerialPort.parsers.readline('\n')
-});
-
-var gyroData;
-
-serialport.on('open', function(){
-    console.log('Serial Port Opend');
-    serialport.on('data', function(data){
-        gyroData = data;
-          console.log(data)
-           httpGet(data);
-    });
-});
+// var SerialPort = require('serialport');
+// var serialport = new SerialPort("/dev/cu.usbmodem1451", {
+//     baudRate: 57600,
+//     parser: SerialPort.parsers.readline('\n')
+// });
+//
+// var gyroData;
+//
+// serialport.on('open', function(){
+//     console.log('Serial Port Opend');
+//     serialport.on('data', function(data){
+//         gyroData = data;
+//           console.log(data)
+//            httpGet(data);
+//     });
+// });
 
 function httpGet(request) {
     var url = "http://localhost:3000/gyro?parameter=";
